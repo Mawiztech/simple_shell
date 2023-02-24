c078b756c5b1fa83e3053563d7bc951fe57640e9
Mawiz <mawiztechnology@gmail.com>
2023-02-24 10:58:06 -0800

    testing shell

---
+++ shell.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+* main - main function the argument
+*
+* @argc: print argument
+* @argv: print the vector
+* Return : return 0
+*/
+
+int main(int argc, char argv[])
+{
+	char betty;
+
+	betty = argv[];
+
+	if (argv[] == 1 && argv[] >= 1)
+	{
+		printf("%c", argv[]);
+	}
+	return (0);
+}
