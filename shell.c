dcea3bfe32531076b12abbaf1704bcc686bae476
Mawiz <mawiztechnology@gmail.com>
2023-02-28 19:00:04 -0800

    hot fix simple shell

---
+++ shell.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- test two numbers
+ *@argc: count
+ *@argv: vector count
+ * Return: o
+ */
+
+int main(int argc, char *argv[])
+{
+	int x = 6;
+	int y = 5;
+	int z = x * y;
+
+	printf("%d", z);
+}
