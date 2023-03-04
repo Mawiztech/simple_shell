c01e23a4c222fab93529d1c20b397aa80e0a802e
Mawiz <mawiztechnology@gmail.com>
2023-03-03 18:19:43 -0800

    task

---
+++ pass.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - add two numbers
+ *
+ *Return: return 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	x = 2;
+	y = 5;
+	z = x + y;
+
+	printf("%d", z);
+
+	return (0);
